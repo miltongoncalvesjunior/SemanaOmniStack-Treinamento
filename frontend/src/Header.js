c835d0f0cbd5224f064cc3146b3b0ece2084168d
miltongoncalvesjunior <decimo.terceiro13@hotmail.com>
2020-03-28 01:46:15 -0300

    Meu app

---
+++ frontend/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Logon from './pages/Logon';
+
+export default function Header(){
+     return (
+        <header>
+         <h1>Be The Hero</h1>
+        </header>   
+    );
+}
